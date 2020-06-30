c2a47bd9af9e52fc932b514cc975050c48783e44
Services2 <Services2@fourge.local>
2020-06-30 17:05:17 +0100

    commit1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Header = (p) => (<h1>{p.course.name}</h1>)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Part = (p) =>(
+  <p>{p.part} {p.excercise}</p>
+)
+const Content = (p) => {
+  return(
+    <div>
+      <Part part = {p.course.parts[0].name} excercise= {p.course.parts[0].exercises}/>
+      <Part part = {p.course.parts[1].name} excercise= {p.course.parts[1].exercises}/>
+      <Part part = {p.course.parts[2].name} excercise= {p.course.parts[2].exercises}/>
+      
+    </div>
+  )
+}
+const Total = (p) => (
+<p>Number of exercises {p.course.parts[0].exercises +p.course.parts[1].exercises+ p.course.parts[2].exercises}</p>
+)
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content course={course}/>
+      <Total course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
